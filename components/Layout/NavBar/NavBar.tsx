ab853fa09086a8980841a4fb5ef56d3d71a83703
Tomek <tomekz@vp.pl>
2022-02-01 15:54:13 +0100

    Add more styling and elements to the NavBar

--- components/Layout/NavBar/NavBar.tsx
+++ components/Layout/NavBar/NavBar.tsx
@@ -1,15 +1,32 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import React, { ReactElement } from 'react';
+import MainLogo from '../../../public/mainLogo.svg';
+import styles from './NavBar.module.css';
 
 export default function Header(): ReactElement {
   return (
     <header>
-      <h1>PlantShop</h1>
-      <nav>
-        <Link href={''}>vbnm</Link>
-        <Link href={''}>123</Link>
-        <Link href={''}>dsfg</Link>
-      </nav>
+      <div className={styles.headerContainer}>
+        <div className={styles.logoContainer}>
+          <Image src={MainLogo.src} width={100} height={100} />
+          <h1>PlantShop</h1>
+        </div>
+        <nav className={styles.navContainer}>
+          <Link href={''}>
+            <a className={styles.navLink}>Home</a>
+          </Link>
+          <Link href={''}>
+            <a className={styles.navLink}>About</a>
+          </Link>
+          <Link href={''}>
+            <a className={styles.navLink}>Products</a>
+          </Link>
+          <Link href={''}>
+            <a className={styles.navLink}>Contact</a>
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
