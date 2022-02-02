c1ec8a59f36b29763c4cf3dc03674b546a13e660
Tomek <tomekz@vp.pl>
2022-02-02 15:54:38 +0100

    Add a user control panel in the NavBar, improve the styling

--- components/Layout/NavBar/NavBar.tsx
+++ components/Layout/NavBar/NavBar.tsx
@@ -2,14 +2,18 @@ import Link from 'next/link';
 import Image from 'next/image';
 import React, { ReactElement } from 'react';
 import MainLogo from '../../../public/mainLogo.svg';
-import styles from './NavBar.module.css';
+import LoginIcon from '../../../public/icons/loginIcon.svg';
+import EmptyBasketIcon from '../../../public/icons/emptyBasketIcon.svg';
+import FullBasketIcon from '../../../public/icons/fullBasketIcon.svg';
+import WishlistIcon from '../../../public/icons/wishlistIcon.svg';
+import styles from './NavBar.module.scss';
 
 export default function Header(): ReactElement {
   return (
     <header>
       <div className={styles.headerContainer}>
         <div className={styles.logoContainer}>
-          <Image src={MainLogo.src} width={100} height={100} />
+          <Image src={MainLogo.src} width={80} height={80} />
           <h1>PlantShop</h1>
         </div>
         <nav className={styles.navContainer}>
@@ -26,6 +30,20 @@ export default function Header(): ReactElement {
             <a className={styles.navLink}>Contact</a>
           </Link>
         </nav>
+        <div className={styles.userControlPanel}>
+          <div>
+            <Image src={WishlistIcon.src} width={40} height={40} />
+            Wishlist (0)
+          </div>
+          <div>
+            <Image src={EmptyBasketIcon.src} width={40} height={40} />
+            Basket
+          </div>
+          <div>
+            <Image src={LoginIcon.src} width={40} height={40} />
+            Login
+          </div>
+        </div>
       </div>
     </header>
   );
