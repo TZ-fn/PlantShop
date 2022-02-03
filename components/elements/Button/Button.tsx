7191c7520807db0828ad47373d5ab463734af7b3
Tomek <tomekz@vp.pl>
2022-02-03 15:50:55 +0100

    Create a button component

---
+++ components/elements/Button/Button.tsx
@@ -0,0 +1,24 @@
+import { ReactElement, ReactNode, MouseEventHandler } from 'react';
+import styles from './Button.module.scss';
+
+interface ButtonProps {
+  children: ReactNode;
+  className?: string;
+  type: 'button' | 'submit' | 'reset' | undefined;
+  ariaLabel?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Button({
+  children,
+  className,
+  type = 'button',
+  ariaLabel,
+  onClick,
+}: ButtonProps): ReactElement {
+  return (
+    <Button className={styles.buttonDefault} aria-label={ariaLabel} type={type} onClick={onClick}>
+      {children}
+    </Button>
+  );
+}
