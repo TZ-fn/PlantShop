cc1a60942f17b3f33ca3a6eb1f2e1c0d95f455eb
Tomek <tomekz@vp.pl>
2022-02-14 15:44:51 +0100

    Enlarge the main logo

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -14,7 +14,7 @@ export default function Header(): ReactElement {
     <header>
       <div className={styles.headerContainer}>
         <div className={styles.logoContainer}>
-          <Image src={MainLogo.src} width={80} height={80} />
+          <Image src={MainLogo.src} width={120} height={120} />
           <h1>PlantShop</h1>
         </div>
         <NavBar />
