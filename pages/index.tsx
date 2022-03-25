7dd6497bab6d524b2cf157a2147bb1dee1089f6e
Tomek <tomekz@vp.pl>
2022-03-25 14:20:16 +0100

    Add a flip effect to the cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import HeadElement from '../components/HeadElement/HeadElement';
 import Header from '../components/Layout/Header/Header';
 import Footer from '../components/Layout/Footer/Footer';
 import styles from '../styles/Home.module.css';
+import Card from '../components/elements/Card/Card';
+import ZamioImage from '../public/images/severin-candrian-cLaaxa4DSnc-unsplash.jpg';
 
 const Home: NextPage = () => {
   return (
@@ -11,9 +13,14 @@ const Home: NextPage = () => {
       <div>
         <Header />
         <main>
-          <h1>PlantShop</h1>
+          <div>
+            <Card name='Zamioculcas' image={ZamioImage.src} />
+            <Card name='Zamioculcas' image={ZamioImage.src} />
+            <Card name='Zamioculcas' image={ZamioImage.src} />
+            <Card name='Zamioculcas' image={ZamioImage.src} />
+          </div>
         </main>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </>
   );
