22abefe6811676551840cdbe8ba1db9e8bb065dd
Tomek <tomekz@vp.pl>
2022-03-29 18:42:40 +0200

    Add a "Turn around" icon to the Cards, improve the styling

--- components/elements/Card/Card.tsx
+++ components/elements/Card/Card.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from 'react';
+import Image from 'next/image';
 import styles from './Card.module.scss';
+import turnAroundIcon from '../../../public/icons/turnAroundArrowIcon.svg';
 
 interface CardProps {
   name: string;
@@ -15,6 +17,9 @@ export default function Card({ name, image, description }: CardProps): ReactElem
           <p className={styles.name}>{name}</p>
           <img className={styles.image} src={image} alt='' />
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <div className={styles.turnIconContainer}>
+            <Image src={turnAroundIcon.src} width={25} height={25} alt='' />
+          </div>
         </div>
         <div className={styles.cardBack}>
           <p>Back dolor sit amet</p>
@@ -23,4 +28,3 @@ export default function Card({ name, image, description }: CardProps): ReactElem
     </div>
   );
 }
-``;
