3845a0e1c18cbe59f14641c6b663f1e077bf6a92
Tomek <tomekz@vp.pl>
2022-04-05 21:22:42 +0200

    Improve the styling, add new card's images

--- components/CardContainer/CardContainer.tsx
+++ components/CardContainer/CardContainer.tsx
@@ -1,14 +1,17 @@
 import { ReactElement } from 'react';
 import Card from '../elements/Card/Card';
 import ZamioImage from '../../public/images/severin-candrian-cLaaxa4DSnc-unsplash.jpg';
+import FortunePlantImage from '../../public/images/rumman-amin-xrYzcMivDSY-unsplash.jpg';
+import ZebraPlantImage from '../../public/images/kari-shea-tcgMBsW4zlU-unsplash.jpg';
 import styles from './CardContainer.module.scss';
 
 export default function CardContainer(): ReactElement {
   return (
     <div className={styles.cardContainer}>
       <Card name='Zamioculcas' image={ZamioImage} />
-      <Card name='Zamioculcas' image={ZamioImage} />
-      <Card name='Zamioculcas' image={ZamioImage} />
+      <Card name='Fortune Plant' image={FortunePlantImage} />
+      <Card name='Zebra haworthia' image={ZebraPlantImage} />
+      <Card name='Fortune Plant' image={FortunePlantImage} />
     </div>
   );
 }
