076cc4c7eff6d5b195905613cd332794d8d0e235
Tomek <tomekz@vp.pl>
2022-04-06 23:01:46 +0200

    Add a new card's picture

--- components/CardContainer/CardContainer.tsx
+++ components/CardContainer/CardContainer.tsx
@@ -3,15 +3,16 @@ import Card from '../elements/Card/Card';
 import ZamioImage from '../../public/images/severin-candrian-cLaaxa4DSnc-unsplash.jpg';
 import FortunePlantImage from '../../public/images/rumman-amin-xrYzcMivDSY-unsplash.jpg';
 import ZebraPlantImage from '../../public/images/kari-shea-tcgMBsW4zlU-unsplash.jpg';
+import MonsteraImage from '../../public/images/severin-candrian-bwsTJMnhcwE-unsplash.jpg';
 import styles from './CardContainer.module.scss';
 
 export default function CardContainer(): ReactElement {
   return (
     <div className={styles.cardContainer}>
       <Card name='Zamioculcas' image={ZamioImage} />
-      <Card name='Fortune Plant' image={FortunePlantImage} />
-      <Card name='Zebra haworthia' image={ZebraPlantImage} />
-      <Card name='Fortune Plant' image={FortunePlantImage} />
+      <Card name='Dracaena' image={FortunePlantImage} />
+      <Card name='Haworthia' image={ZebraPlantImage} />
+      <Card name='Monstera' image={MonsteraImage} />
     </div>
   );
 }
