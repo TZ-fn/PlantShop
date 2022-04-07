165b8369a1b5ca7a994a413734ab1681c742c077
Tomek <tomekz@vp.pl>
2022-04-07 19:21:10 +0200

    Add more plants' pictures, improve the styling.

--- components/CardContainer/CardContainer.tsx
+++ components/CardContainer/CardContainer.tsx
@@ -4,6 +4,11 @@ import ZamioImage from '../../public/images/severin-candrian-cLaaxa4DSnc-unsplas
 import FortunePlantImage from '../../public/images/rumman-amin-xrYzcMivDSY-unsplash.jpg';
 import ZebraPlantImage from '../../public/images/kari-shea-tcgMBsW4zlU-unsplash.jpg';
 import MonsteraImage from '../../public/images/severin-candrian-bwsTJMnhcwE-unsplash.jpg';
+import SansevieriaImage from '../../public/images/severin-candrian-NlY6jBS3taA-unsplash.jpg';
+import EpipremnumImage from '../../public/images/severin-candrian-PKQHPDetrtc-unsplash.jpg';
+import DwarfBananaImage from '../../public/images/severin-candrian-rOfBozD9pOE-unsplash.jpg';
+import CalatheaImage from '../../public/images/severin-candrian-KU-lo3dR-K4-unsplash.jpg';
+
 import styles from './CardContainer.module.scss';
 
 export default function CardContainer(): ReactElement {
@@ -11,8 +16,12 @@ export default function CardContainer(): ReactElement {
     <div className={styles.cardContainer}>
       <Card name='Zamioculcas' image={ZamioImage} />
       <Card name='Dracaena' image={FortunePlantImage} />
-      <Card name='Haworthia' image={ZebraPlantImage} />
       <Card name='Monstera' image={MonsteraImage} />
+      <Card name='Sansevieria' image={SansevieriaImage} />
+      <Card name='Epipremnum' image={EpipremnumImage} />
+      <Card name='Dwarf banana' image={DwarfBananaImage} />
+      <Card name='Haworthia' image={ZebraPlantImage} />
+      <Card name='Calathea' image={CalatheaImage} />
     </div>
   );
 }
