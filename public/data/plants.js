40e99acd4d79286ada6b70747486e5bea7ab3a77
Tomek <tomekz@vp.pl>
2022-04-12 20:28:39 +0200

    Add an api route returning all the plants

---
+++ public/data/plants.js
@@ -0,0 +1,53 @@
+import ZamioImage from '../../public/images/severin-candrian-cLaaxa4DSnc-unsplash.jpg';
+import FortunePlantImage from '../../public/images/rumman-amin-xrYzcMivDSY-unsplash.jpg';
+import ZebraPlantImage from '../../public/images/kari-shea-tcgMBsW4zlU-unsplash.jpg';
+import MonsteraImage from '../../public/images/severin-candrian-bwsTJMnhcwE-unsplash.jpg';
+import SansevieriaImage from '../../public/images/severin-candrian-NlY6jBS3taA-unsplash.jpg';
+import EpipremnumImage from '../../public/images/severin-candrian-PKQHPDetrtc-unsplash.jpg';
+import DwarfBananaImage from '../../public/images/severin-candrian-rOfBozD9pOE-unsplash.jpg';
+import CalatheaImage from '../../public/images/severin-candrian-KU-lo3dR-K4-unsplash.jpg';
+
+const plants = [
+  {
+    name: 'Zamioculcas',
+    image: ZamioImage.src,
+    description: '',
+  },
+  {
+    name: 'Dracaena',
+    image: FortunePlantImage.src,
+    description: '',
+  },
+  {
+    name: 'Monstera',
+    image: MonsteraImage.src,
+    description: '',
+  },
+  {
+    name: 'Sansevieria',
+    image: SansevieriaImage.src,
+    description: '',
+  },
+  {
+    name: 'Epipremnum',
+    image: EpipremnumImage.src,
+    description: '',
+  },
+  {
+    name: 'Dwarf banana',
+    image: DwarfBananaImage.src,
+    description: '',
+  },
+  {
+    name: 'Haworthia',
+    image: ZebraPlantImage.src,
+    description: '',
+  },
+  {
+    name: 'Calathea',
+    image: CalatheaImage.src,
+    description: '',
+  },
+];
+
+export default plants;
