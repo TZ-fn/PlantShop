eb53a5bb58f4ba20eea72d2fb60dba759a89b781
Tomek <tomekz@vp.pl>
2022-04-15 16:52:56 +0200

    Add typing to the plants' data

--- components/CardContainer/CardContainer.tsx
+++ components/CardContainer/CardContainer.tsx
@@ -2,15 +2,17 @@ import { ReactElement } from 'react';
 import Card from '../elements/Card/Card';
 import styles from './CardContainer.module.scss';
 import { useFetch } from '../../hooks/useFetch';
+import { Plant, PlantData } from '../../types/types';
 
 export default function CardContainer(): ReactElement {
-  const data = useFetch('/api/plants');
+  const data: PlantData = useFetch('/api/plants');
   return (
     <div className={styles.cardContainer}>
-      {data &&
-        data.map((plant) => {
-          return <Card name={plant.name} image={plant.image} description={plant.description} />;
-        })}
+      {data !== undefined
+        ? data.map((plant: Plant) => {
+            return <Card name={plant.name} image={plant.image} description={plant.description} />;
+          })
+        : 'Loading'}
     </div>
   );
 }
