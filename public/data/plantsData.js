d2c4ee783cd60945437a680a8e5cebba53415444
Tomek <tomekz@vp.pl>
2022-04-18 21:00:25 +0200

    Change the links to plants' images and rename the file for a better readability

---
+++ public/data/plantsData.js
@@ -0,0 +1,44 @@
+const plants = [
+  {
+    name: 'Zamioculcas',
+    image: '/images/severin-candrian-cLaaxa4DSnc-unsplash.jpg',
+    description: '',
+  },
+  {
+    name: 'Dracaena',
+    image: '/images/rumman-amin-xrYzcMivDSY-unsplash.jpg',
+    description: '',
+  },
+  {
+    name: 'Monstera',
+    image: '/images/severin-candrian-bwsTJMnhcwE-unsplash.jpg',
+    description: '',
+  },
+  {
+    name: 'Sansevieria',
+    image: '/images/severin-candrian-NlY6jBS3taA-unsplash.jpg',
+    description: '',
+  },
+  {
+    name: 'Epipremnum',
+    image: '/images/severin-candrian-PKQHPDetrtc-unsplash.jpg',
+    description: '',
+  },
+  {
+    name: 'Dwarf banana',
+    image: '/images/severin-candrian-rOfBozD9pOE-unsplash.jpg',
+    description: '',
+  },
+  {
+    name: 'Haworthia',
+    image: '/images/kari-shea-tcgMBsW4zlU-unsplash.jpg',
+    description: '',
+  },
+  {
+    name: 'Calathea',
+    image: '/images/severin-candrian-KU-lo3dR-K4-unsplash.jpg',
+    description: '',
+  },
+];
+
+export default plants;
