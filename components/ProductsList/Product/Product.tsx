65a88a789be90946ebaa0e08199ebff8c5d904e5
Tomek <tomekz@vp.pl>
2022-04-20 21:57:28 +0200

    Add a visually-hidden class, add accessible descriptions for the elements in Product component

--- components/ProductsList/Product/Product.tsx
+++ components/ProductsList/Product/Product.tsx
@@ -7,16 +7,25 @@ export default function Product(): ReactElement {
       <p className={styles.productName}>Lorem ipsum dolor sit amet</p>
       <div className={styles.counter}>
         <button type='button' className={styles.countMinus}>
-          -
+          - <span className='visually-hidden'>Remove 1 of the item</span>
         </button>
-        <input type='text' className={styles.count} />
+        <label htmlFor='item-count'>
+          <span className='visually-hidden'>Count of the item</span>
+          <input
+            name='item-count'
+            autoComplete='off'
+            title=''
+            type='text'
+            className={styles.count}
+          />
+        </label>
         <button type='button' className={styles.countPlus}>
-          +
+          + <span className='visually-hidden'>Add 1 of the item</span>
         </button>
       </div>
       <p className={styles.price}>123123</p>
       <button type='button' className={styles.removeButton}>
-        remove
+        <span className='visually-hidden'>Remove item from the basket</span>
       </button>
     </li>
   );
