7d61cf902f11365db9ede837164b0db6b3e099e6
Tomek <tomekz@vp.pl>
2022-04-22 17:57:04 +0200

    Create an aside element in the Basket component

---
+++ components/BasketAside/BasketAside.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+import styles from './BasketAside.module.scss';
+
+export default function BasketAside(): ReactElement {
+  return (
+    <aside className={styles.asideContainer}>
+      <button type='button' className={styles.checkoutButton}>
+        Checkout and pay
+      </button>
+      <button type='button' className={styles.continueButton}>
+        Continue shopping
+      </button>
+    </aside>
+  );
+}
