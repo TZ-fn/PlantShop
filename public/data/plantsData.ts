a58ac8ab4fd55850cea2e407aec797be4c05d8c8
Tomek <tomekz@vp.pl>
2022-04-29 19:32:39 +0200

    Refactor the types to uniform them, add ID property to the plants' data

--- public/data/plantsData.ts
+++ public/data/plantsData.ts
@@ -1,59 +1,61 @@
-const plants = [
+import { PlantsData } from '../../types/types';
+
+const plants: PlantsData = [
   {
+    id: 'lga8HRSSv0y2',
     name: 'Zamioculcas',
     image: '/images/severin-candrian-cLaaxa4DSnc-unsplash.jpg',
     description: '',
-    price: { integer: 14, fraction: 99 },
-    currency: 'USD',
+    price: { integer: 14, fraction: 99, currency: 'USD' },
   },
   {
+    id: 'btuydewwyHc3',
     name: 'Dracaena',
     image: '/images/rumman-amin-xrYzcMivDSY-unsplash.jpg',
     description: '',
-    price: { integer: 10, fraction: 33 },
-    currency: 'USD',
+    price: { integer: 10, fraction: 33, currency: 'USD' },
   },
   {
+    id: '1ivJTOlbMXoS',
     name: 'Monstera',
     image: '/images/severin-candrian-bwsTJMnhcwE-unsplash.jpg',
     description: '',
-    price: { integer: 9, fraction: 89 },
-    currency: 'USD',
+    price: { integer: 9, fraction: 89, currency: 'USD' },
   },
   {
+    id: 'Y2bkNa3joxp3',
     name: 'Sansevieria',
     image: '/images/severin-candrian-NlY6jBS3taA-unsplash.jpg',
     description: '',
-    price: { integer: 20, fraction: 90 },
-    currency: 'USD',
+    price: { integer: 20, fraction: 90, currency: 'USD' },
   },
   {
+    id: 'aFITm20xiHrZ',
     name: 'Epipremnum',
     image: '/images/severin-candrian-PKQHPDetrtc-unsplash.jpg',
     description: '',
-    price: { integer: 8, fraction: 0 },
-    currency: 'USD',
+    price: { integer: 8, fraction: 0, currency: 'USD' },
   },
   {
+    id: 'Zi2qhMROdxTp',
     name: 'Dwarf banana',
     image: '/images/severin-candrian-rOfBozD9pOE-unsplash.jpg',
     description: '',
-    price: { integer: 25, fraction: 5 },
-    currency: 'USD',
+    price: { integer: 25, fraction: 5, currency: 'USD' },
   },
   {
+    id: 'mibsfliS6VlH',
     name: 'Haworthia',
     image: '/images/kari-shea-tcgMBsW4zlU-unsplash.jpg',
     description: '',
-    price: { integer: 11, fraction: 49 },
-    currency: 'USD',
+    price: { integer: 11, fraction: 49, currency: 'USD' },
   },
   {
+    id: 'E4AvsohDvcx7',
     name: 'Calathea',
     image: '/images/severin-candrian-KU-lo3dR-K4-unsplash.jpg',
     description: '',
-    price: { integer: 23, fraction: 19 },
-    currency: 'USD',
+    price: { integer: 23, fraction: 19, currency: 'USD' },
   },
 ];
 
