a11eaba534aaf1c06f0f405827228facf356a5cc
Tomek <tomekz@vp.pl>
2022-05-01 17:01:38 +0200

    Add links to the Header component

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import MainLogo from '../../../public/mainLogo.svg';
 import NavBar from './NavBar/NavBar';
 import SearchBar from './SearchBar/SearchBar';
@@ -13,25 +14,36 @@ export default function Header(): ReactElement {
   return (
     <header>
       <div className={styles.headerContainer}>
-        <div className={styles.logoContainer}>
-          <Image src={MainLogo.src} width={100} height={100} />
-          <h1>PlantShop</h1>
-        </div>
+        <Link href={'/'}>
+          <div className={styles.logoContainer}>
+            <Image src={MainLogo.src} width={100} height={100} />
+            <h1>PlantShop</h1>
+          </div>
+        </Link>
+
         <NavBar />
+
         <SearchBar />
+
         <div className={styles.userControlPanel}>
-          <div>
-            <Image src={WishlistIcon.src} width={40} height={40} />
-            Wishlist (0)
-          </div>
-          <div>
-            <Image src={EmptyBasketIcon.src} width={40} height={40} />
-            Basket
-          </div>
-          <div>
-            <Image src={LoginIcon.src} width={40} height={40} />
-            Login
-          </div>
+          <Link href={'/wishlist'}>
+            <div>
+              <Image src={WishlistIcon.src} width={40} height={40} />
+              Wishlist (0)
+            </div>
+          </Link>
+          <Link href={'/basket'}>
+            <div>
+              <Image src={EmptyBasketIcon.src} width={40} height={40} />
+              Basket
+            </div>
+          </Link>
+          <Link href={'/login'}>
+            <div>
+              <Image src={LoginIcon.src} width={40} height={40} />
+              Login
+            </div>
+          </Link>
         </div>
       </div>
     </header>
