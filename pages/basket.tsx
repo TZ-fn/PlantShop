afb725c60e4f2b822a2294deb0b900ab33c4a28d
Tomek <tomekz@vp.pl>
2022-05-04 21:05:27 +0200

    Add a Layout component containing the Head element, Header and Footer components.

--- pages/basket.tsx
+++ pages/basket.tsx
@@ -1,24 +1,14 @@
 import type { NextPage } from 'next';
 import styles from './basket.module.scss';
-import HeadElement from '../components/HeadElement/HeadElement';
-import Header from '../components/Layout/Header/Header';
-import Footer from '../components/Layout/Footer/Footer';
 import ProductsList from '../components/ProductsList/ProductsList';
 import BasketAside from '../components/BasketAside/BasketAside';
 
 const Basket: NextPage = () => {
   return (
-    <>
-      <HeadElement />
-      <div>
-        <Header />
-        <div className={styles.container}>
-          <ProductsList />
-          <BasketAside />
-        </div>
-        {/* <Footer /> */}
-      </div>
-    </>
+    <div className={styles.basketContainer}>
+      <ProductsList />
+      <BasketAside />
+    </div>
   );
 };
 
