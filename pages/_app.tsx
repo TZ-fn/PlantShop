df3402e1b44ec67169a5a88c0456ca8802736cdf
Tomek <tomekz@vp.pl>
2022-05-05 10:33:07 +0200

    Move the HeadElement inside the Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import '../styles/variables.scss';
 import Layout from '../components/Layout/Layout';
-import HeadElement from '../components/HeadElement/HeadElement';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <HeadElement />
       <Component {...pageProps} />
     </Layout>
   );
