d2fab65de0953c40f8b97b1f56bc3d89178bccc4
Tomek <tomekz@vp.pl>
2022-05-05 10:33:07 +0200

    Move the HeadElement inside the Layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import HeadElement from '../HeadElement/HeadElement';
 
 export default function Layout({ children }) {
   return (
     <>
+      <HeadElement />
       <Header />
       {children}
       <Footer />
