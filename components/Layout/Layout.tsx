68d9652050d28ebe5929c8d80af1d8fd67f0e850
Tomek <tomekz@vp.pl>
2022-05-05 14:30:13 +0200

    Add typing to the children props in the Layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,8 +1,13 @@
+import { ReactNode } from 'react';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import HeadElement from '../HeadElement/HeadElement';
 
-export default function Layout({ children }) {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <HeadElement />
