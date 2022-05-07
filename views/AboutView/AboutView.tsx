bae9684f6f6ab966d48b69e3c05f5efba4c3debc
Tomek <tomekz@vp.pl>
2022-05-07 21:26:47 +0200

    Create an About route and component

---
+++ views/AboutView/AboutView.tsx
@@ -0,0 +1,11 @@
+import styles from './AboutView.module.scss';
+
+const AboutView = () => {
+  return (
+    <div className={styles.aboutContainer}>
+      <h2 className={styles.aboutHeader}>About</h2>
+    </div>
+  );
+};
+
+export default AboutView;
