58f6c37957342843b39c4d881fe610acce466712
Tomek <tomekz@vp.pl>
2022-05-13 14:18:19 +0200

    Add more styling and logic to the Login view

--- views/LoginView/LoginView.tsx
+++ views/LoginView/LoginView.tsx
@@ -1,12 +1,44 @@
+import Input from 'components/Input/Input';
 import { useState } from 'react';
 import styles from './LoginView.module.scss';
 
 export default function LoginView() {
   const [isLoginPage, setIsLoginPage] = useState(false);
+
+  function handleTabButtonClick(e) {
+    return e.target.id === 'registerTab' ? setIsLoginPage(false) : setIsLoginPage(true);
+  }
+
   return (
     <div className={styles.mainContainer}>
+      {!isLoginPage && (
+        <div className={styles.registerContainer}>
+          <Input id='email' type='text' placeholder='email' label='E-mail' />
+          <Input id='password' type='text' placeholder='password' label='Password' />
+          <button type='button' className={styles.registerButton}>
+            Register
+          </button>
+        </div>
+      )}
       {isLoginPage && <div className={styles.loginContainer}>login</div>}
-      {!isLoginPage && <div className={styles.registerContainer}>register</div>}
+      <div className={styles.tabsContainer}>
+        <button
+          id='registerTab'
+          type='button'
+          className={`${styles.tab} ${!isLoginPage && styles.active}`}
+          onClick={(e) => handleTabButtonClick(e)}
+        >
+          Register
+        </button>
+        <button
+          id='loginTab'
+          type='button'
+          className={`${styles.tab} ${isLoginPage && styles.active}`}
+          onClick={(e) => handleTabButtonClick(e)}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 }
