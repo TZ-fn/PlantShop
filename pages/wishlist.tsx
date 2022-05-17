c14aeb35b6780f1206f57c13047201cf2095cbb2
Tomek <tomekz@vp.pl>
2022-05-17 16:49:04 +0200

    Add a Wishlist view and route

---
+++ pages/wishlist.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import WishlistView from 'views/WishlistView/WishlistView';
+
+const Wishlist: NextPage = () => {
+  return <WishlistView />;
+};
+
+export default Wishlist;
