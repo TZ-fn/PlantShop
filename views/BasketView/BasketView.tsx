f12982ee27b721be454c05f6a5a7916f7cef5c30
Tomek <tomekz@vp.pl>
2022-05-18 21:46:14 +0200

    Refactor the ProductsList component, it now renders it's children as a list

--- views/BasketView/BasketView.tsx
+++ views/BasketView/BasketView.tsx
@@ -1,11 +1,18 @@
 import styles from './BasketView.module.scss';
 import ProductsList from 'components/ProductsList/ProductsList';
+import Product from 'components/ProductsList/Product/Product';
 import BasketAside from 'components/BasketAside/BasketAside';
 
 const BasketView = () => {
   return (
     <div className={styles.basketContainer}>
-      <ProductsList />
+      <ProductsList>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </ProductsList>
       <BasketAside />
     </div>
   );
