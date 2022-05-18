dbf5840b1f423eea2e358f871d0f0bf7a89dea61
Tomek <tomekz@vp.pl>
2022-05-18 21:46:14 +0200

    Refactor the ProductsList component, it now renders it's children as a list

--- components/ProductsList/ProductsList.tsx
+++ components/ProductsList/ProductsList.tsx
@@ -1,16 +1,6 @@
 import { ReactElement } from 'react';
 import styles from './ProductsList.module.scss';
-import Product from './Product/Product';
 
-export default function ProductsList(): ReactElement {
-  return (
-    <ul className={styles.list}>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-    </ul>
-  );
+export default function ProductsList({ children }): ReactElement {
+  return <ul className={styles.list}>{children}</ul>;
 }
