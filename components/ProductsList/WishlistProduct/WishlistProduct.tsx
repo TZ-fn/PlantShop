b993ea625aec83b394e36db2522cd93d21b98418
Tomek <tomekz@vp.pl>
2022-05-18 21:47:15 +0200

    Create a WishlistProduct component for the WishlistView

---
+++ components/ProductsList/WishlistProduct/WishlistProduct.tsx
@@ -0,0 +1,62 @@
+import { ChangeEvent, ReactElement, useState } from 'react';
+import styles from './WishlistProduct.module.scss';
+import Image from 'next/image';
+import BinIcon from 'public/icons/binIcon.svg';
+import { formatCurrency } from 'utils/formatNumber';
+import { ProductProps } from 'types/types';
+
+export default function WishlistProduct({ id, name, count, price }: ProductProps): ReactElement {
+  const [countValue, setCountValue] = useState('1');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCountValue(e.target.value);
+  };
+
+  const handleCountButtons = (operationType: string) => {
+    if (countValue === '0' && operationType === 'decrease') {
+      return;
+    }
+    return operationType === 'increase'
+      ? setCountValue(String(Number(countValue) + 1))
+      : setCountValue(String(Number(countValue) - 1));
+  };
+
+  return (
+    <li className={styles.productContainer}>
+      <p className={styles.productName}>Lorem ipsum dolor sit amet</p>
+      <div className={styles.counter}>
+        <button
+          type='button'
+          className={styles.countMinus}
+          onClick={() => handleCountButtons('decrease')}
+        >
+          - <span className='visually-hidden'>Remove 1 of this item</span>
+        </button>
+        <label htmlFor='item-count'>
+          <span className='visually-hidden'>Count of the item</span>
+          <input
+            name='item-count'
+            autoComplete='off'
+            title=''
+            type='number'
+            value={countValue}
+            onChange={(e) => handleInputChange(e)}
+            className={styles.count}
+          />
+        </label>
+        <button
+          type='button'
+          className={styles.countPlus}
+          onClick={() => handleCountButtons('increase')}
+        >
+          + <span className='visually-hidden'>Add 1 of this item</span>
+        </button>
+      </div>
+      <p className={styles.price}>Total: {formatCurrency(123123.12)}</p>
+      <button type='button' className={styles.removeButton}>
+        <span className='visually-hidden'>Remove item from the basket</span>
+        <Image src={BinIcon.src} width={'40px'} height={'40px'} alt='' layout='fixed' />
+      </button>
+    </li>
+  );
+}
--- views/WishlistView/WishlistView.tsx
+++ views/WishlistView/WishlistView.tsx
@@ -1,5 +1,17 @@
+import ProductsList from 'components/ProductsList/ProductsList';
+import Product from 'components/ProductsList/Product/Product';
 import styles from './WishlistView.module.scss';
 
 export default function WishlistView() {
-  return <div className={styles.wishlistContainer}>Wishlist</div>;
+  return (
+    <div className={styles.wishlistContainer}>
+      <ProductsList>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </ProductsList>
+    </div>
+  );
 }
