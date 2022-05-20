3c5e20dac0c17bb351e848b94ffdb0e3776f494d
Tomek <tomekz@vp.pl>
2022-05-20 16:10:15 +0200

    Add a section header

--- views/BasketView/BasketView.tsx
+++ views/BasketView/BasketView.tsx
@@ -5,15 +5,18 @@ import BasketAside from 'components/BasketAside/BasketAside';
 
 const BasketView = () => {
   return (
-    <div className={styles.basketContainer}>
-      <ProductsList>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-      </ProductsList>
-      <BasketAside />
+    <div className={styles.basketViewContainer}>
+      <h2 className={styles.sectionHeader}>Basket</h2>
+      <div className={styles.basketContainer}>
+        <ProductsList>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+          <Product />
+        </ProductsList>
+        <BasketAside />
+      </div>
     </div>
   );
 };
