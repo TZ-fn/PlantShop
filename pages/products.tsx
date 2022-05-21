5fb1c162e0d9efc106a7f2bc156e4858a4b0e75a
Tomek <tomekz@vp.pl>
2022-05-21 19:43:15 +0200

    Create a Products route and view

---
+++ pages/products.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import ProductsView from 'views/ProductsView/ProductsView';
+
+const Products: NextPage = () => {
+  return <ProductsView />;
+};
+
+export default Products;
---
+++ views/ProductsView/ProductsView.tsx
@@ -0,0 +1,9 @@
+import styles from './ProductsView.module.scss';
+
+export default function ProductsView() {
+  return (
+    <div className={styles.productsContainer}>
+      <h2 className={styles.sectionHeader}>Products</h2>
+    </div>
+  );
+}
