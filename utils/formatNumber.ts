d3843b311680c409eaf1fc1d0894b02978055d6e
Tomek <tomekz@vp.pl>
2022-05-25 18:01:09 +0200

    Refactor the formatNumber function so it's argument matches the data from the API

--- utils/formatNumber.ts
+++ utils/formatNumber.ts
@@ -1,14 +1,11 @@
 export function formatCurrency(
-  amount: number | string,
+  { integer, fraction, currency = 'USD' }: { integer: number; fraction: number; currency: string },
   locale: string = 'en-US',
-  currencyType: string = 'USD',
 ) {
-  if (typeof amount === 'string') {
-    amount = Number(amount);
-  }
+  const amount = integer + fraction * 0.01;
   return new Intl.NumberFormat(locale, {
     style: 'currency',
-    currency: currencyType,
+    currency: currency,
     currencyDisplay: 'narrowSymbol',
   }).format(amount);
 }
