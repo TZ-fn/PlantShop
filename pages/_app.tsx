2ae1d8394fa0b43b5e1f22d85e0fc469bf612b65
Tomek <tomekz@vp.pl>
2022-05-29 20:51:49 +0200

    Add and configure the store and provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from 'components/store/store';
 import Layout from 'components/Layout/Layout';
 import 'styles/globals.css';
 import 'styles/variables.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
