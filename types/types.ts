c44ebb84ae5678cfd64260943c37e770017b4623
Tomek <tomekz@vp.pl>
2022-05-31 16:19:37 +0200

    Add an Product interface to be used in the Basket component

--- types/types.ts
+++ types/types.ts
@@ -6,6 +6,10 @@ export interface Plant {
   price: { integer: number; fraction: number; currency: string };
 }
 
+export interface Product extends Plant {
+  quantity: number;
+}
+
 export type PlantsData = Plant[];
 export interface ProductProps extends Plant {
   count: number;
