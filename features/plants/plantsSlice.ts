97fca6515e36929eef62a56a121c4032a1f93ecb
Tomek <tomekz@vp.pl>
2022-06-04 20:51:57 +0200

    Add a plants slice to handle the plants' data

---
+++ features/plants/plantsSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { PlantsData } from 'types/types';
+
+export interface PlantsState {
+  plantsData: PlantsData;
+}
+
+const initialState: PlantsState = {
+  plantsData: [],
+};
+
+export const plantsSlice = createSlice({
+  name: 'plants',
+  initialState,
+  reducers: {
+    updatePlantsData: async ({ plantsData }, action: PayloadAction<PlantsData>) => {
+      console.log(await action.payload);
+      plantsData = await action.payload;
+    },
+  },
+});
+
+export const { updatePlantsData } = plantsSlice.actions;
+
+export default plantsSlice.reducer;
