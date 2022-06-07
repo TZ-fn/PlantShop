5f6115327c041939db546bc75362824bb901ffeb
Tomek <tomekz@vp.pl>
2022-06-07 15:56:18 +0200

    Move the fetching logic to the Layout component, this fixes the infinite re-render loop

--- components/CardContainer/CardContainer.tsx
+++ components/CardContainer/CardContainer.tsx
@@ -1,18 +1,12 @@
 import { ReactElement } from 'react';
 import { RootState } from 'store/store';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Card from 'components/elements/Card/Card';
 import styles from './CardContainer.module.scss';
-import { useFetch } from 'hooks/useFetch';
 import { Plant } from 'types/types';
-import { updatePlantsData } from 'features/plants/plantsSlice';
 
 export default function CardContainer(): ReactElement {
-  const dispatch = useDispatch();
-  const data = useFetch('/api/plants');
-  dispatch(updatePlantsData(data));
   const plants = useSelector((state: RootState) => state.plants.plantsData);
-  console.log(plants);
   return (
     <div className={styles.cardContainer}>
       {plants.length > 0
