403ce3bad59f325e683af8c2d84c315d74a45604
Tomek <tomekz@vp.pl>
2022-06-07 16:47:37 +0200

    Refactor the formatCurrency function to calculate the total amount when there is more than one item

--- utils/formatCurrency.ts
+++ utils/formatCurrency.ts
@@ -1,8 +1,9 @@
 export function formatCurrency(
   { integer, fraction, currency = 'USD' }: { integer: number; fraction: number; currency: string },
   locale: string = 'en-US',
+  count: number = 1,
 ) {
-  const amount = integer + fraction * 0.01;
+  const amount = (integer + fraction * 0.01) * count;
   return new Intl.NumberFormat(locale, {
     style: 'currency',
     currency: currency,
