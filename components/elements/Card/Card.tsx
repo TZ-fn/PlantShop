2231d43884d15857c63f08b4a56b33f58b8856d8
Tomek <tomekz@vp.pl>
2022-06-07 16:35:43 +0200

    Rename the formatCurrency file to match the exported function's name

--- components/elements/Card/Card.tsx
+++ components/elements/Card/Card.tsx
@@ -5,7 +5,7 @@ import { addToBasket } from 'features/basket/basketSlice';
 import styles from './Card.module.scss';
 import turnAroundIcon from 'public/icons/turnAroundArrowIcon.svg';
 import addToBasketIcon from 'public/icons/addToBasketIcon.svg';
-import { formatCurrency } from 'utils/formatNumber';
+import { formatCurrency } from 'utils/formatCurrency';
 import { Plant } from 'types/types';
 
 export default function Card({ id, name, image, description, price }: Plant): ReactElement {
