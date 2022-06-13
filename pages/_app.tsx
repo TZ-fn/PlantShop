bfe0973cd96d6f8d8606842d0bcee41d98d1a59c
Tomek <tomekz@vp.pl>
2022-06-13 20:18:14 +0200

    Add the toastify provider to the root of the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { store } from 'store/store';
 import Layout from 'components/Layout/Layout';
 import 'styles/globals.css';
@@ -8,6 +10,7 @@ import 'styles/variables.scss';
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <ToastContainer />
       <Layout>
         <Component {...pageProps} />
       </Layout>
