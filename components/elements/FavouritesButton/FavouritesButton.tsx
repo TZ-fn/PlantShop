73d15dcd53e0ede84131822538a6eb9685572f26
Tomek <tomekz@vp.pl>
2022-06-24 06:56:53 +0200

    Add a changing icon logic for the FavouritesButton

--- components/elements/FavouritesButton/FavouritesButton.tsx
+++ components/elements/FavouritesButton/FavouritesButton.tsx
@@ -1,4 +1,7 @@
 import { ReactElement } from 'react';
+import Image from 'next/image';
+import WishlistIcon from 'public/icons/wishlistIcon.svg';
+import WishlistIconEmpty from 'public/icons/wishlistIconEmpty.svg';
 import Button from '../Button/Button';
 import styles from './FavouritesButton.module.scss';
 
@@ -17,7 +20,17 @@ export default function FavouritesButton({ isWishlisted }: FavouritesButtonProps
         type='button'
         onClick={handleAddingToFavourites}
       >
-        {isWishlisted ? true : false}
+        {isWishlisted ? (
+          <span>
+            <span className='visually-hidden'>Remove this item from the wishlist</span>
+            <Image src={WishlistIcon.src} width={50} height={50} />
+          </span>
+        ) : (
+          <span>
+            <Image src={WishlistIconEmpty.src} width={50} height={50} />
+            <span className='visually-hidden'>Remove this item from the wishlist</span>
+          </span>
+        )}
       </Button>
     </div>
   );
