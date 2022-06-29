40eb78ba578cb411f6cb19e3f71d4ebbb4caea5b
Tomek <tomekz@vp.pl>
2022-06-29 17:00:43 +0200

    Displaying items in the Wishlist component now works properly

--- components/ProductsList/WishlistProduct/WishlistProduct.tsx
+++ components/ProductsList/WishlistProduct/WishlistProduct.tsx
@@ -1,15 +1,17 @@
-import { ChangeEvent, ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
 import styles from './WishlistProduct.module.scss';
 import Image from 'next/image';
 import BinIcon from 'public/icons/binIcon.svg';
+import ProductMiniature from 'components/elements/ProductMiniature/ProductMiniature';
 import { formatCurrency } from 'utils/formatCurrency';
 import { ProductProps } from 'types/types';
 
-export default function WishlistProduct({ id, name, price }: ProductProps): ReactElement {
+export default function WishlistProduct({ name, image, price }: ProductProps): ReactElement {
   return (
     <li className={styles.productContainer}>
-      <p className={styles.productName}>Lorem ipsum dolor sit amet</p>
-      <p className={styles.price}>Price: {formatCurrency(12.12)}</p>
+      <ProductMiniature source={image} />
+      <p className={styles.productName}>{name}</p>
+      <p className={styles.price}>Price: {formatCurrency(price)}</p>
       <button type='button' className={styles.removeButton}>
         <span className='visually-hidden'>Remove item from the wishlist</span>
         <Image src={BinIcon.src} width={'40px'} height={'40px'} alt='' layout='fixed' />
