3b020266d3d6e26671dc54d4379af9a23002a436
Tomek <tomekz@vp.pl>
2022-06-29 17:00:43 +0200

    Displaying items in the Wishlist component now works properly

--- views/WishlistView/WishlistView.tsx
+++ views/WishlistView/WishlistView.tsx
@@ -1,17 +1,23 @@
 import ProductsList from 'components/ProductsList/ProductsList';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/store';
 import WishlistProduct from 'components/ProductsList/WishlistProduct/WishlistProduct';
 import styles from './WishlistView.module.scss';
 
 export default function WishlistView() {
+  const plants = useSelector((state: RootState) => state.plants.plantsData);
+  const wishlist = useSelector((state: RootState) => state.wishlist.products);
+
+  console.log(plants.filter((plant) => wishlist.includes(plant.id)));
   return (
     <div className={styles.wishlistContainer}>
       <h2 className={styles.sectionHeader}>WishList</h2>
       <ProductsList>
-        <WishlistProduct />
-        <WishlistProduct />
-        <WishlistProduct />
-        <WishlistProduct />
-        <WishlistProduct />
+        {plants
+          .filter((plant) => wishlist.includes(plant.id))
+          .map(({ name, image, price }) => {
+            return <WishlistProduct name={name} image={image} price={price} />;
+          })}
       </ProductsList>
     </div>
   );
