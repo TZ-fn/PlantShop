4853a21403f09e904345bd132fe67042fdc73c48
Tomek <tomekz@vp.pl>
2022-07-01 15:59:33 +0200

    Removing products from the wishlist in a wishlist view now works properly, remove obsolete console.logs

--- views/WishlistView/WishlistView.tsx
+++ views/WishlistView/WishlistView.tsx
@@ -8,7 +8,6 @@ export default function WishlistView() {
   const plants = useSelector((state: RootState) => state.plants.plantsData);
   const wishlist = useSelector((state: RootState) => state.wishlist.products);
 
-  console.log(plants.filter((plant) => wishlist.includes(plant.id)));
   return (
     <div className={styles.wishlistContainer}>
       <h2 className={styles.sectionHeader}>WishList</h2>
@@ -16,8 +15,8 @@ export default function WishlistView() {
         {wishlist.length > 0 ? (
           plants
             .filter((plant) => wishlist.includes(plant.id))
-            .map(({ name, image, price }) => {
-              return <WishlistProduct name={name} image={image} price={price} />;
+            .map(({ id, name, image, price }) => {
+              return <WishlistProduct key={id} id={id} name={name} image={image} price={price} />;
             })
         ) : (
           <p className={styles.emptyWishlist}>Your wishlist is empty. ☹️</p>
