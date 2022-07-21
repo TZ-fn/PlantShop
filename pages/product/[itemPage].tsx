4b600d1b18f64dd43a11667f134670495ef8badf
Tomek <tomekz@vp.pl>
2022-07-21 21:59:30 +0200

    Dynamic routing to products' details now works properly

--- pages/product/[itemPage].tsx
+++ pages/product/[itemPage].tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
+import capitalize from 'utils/capitalize';
 import ItemView from 'views/ItemView/ItemView';
 
 const ItemPage: NextPage = () => {
+  const router = useRouter();
+  const product = capitalize(router.query.itemPage);
   const plants = useSelector((state: RootState) => state.plants.plantsData);
-  // remove later on
-  const mockedPlant = plants[0];
+  const currentPlant = plants.filter((plant) => plant.name === product)[0];
 
-  return mockedPlant !== undefined ? <ItemView {...mockedPlant} /> : <p>Loading...</p>;
+  return currentPlant !== undefined ? <ItemView {...currentPlant} /> : <p>Loading...</p>;
 };
 
 export default ItemPage;
