2321b0710b43e0aa76d104fde0d96d1e07821277
Tomek <tomekz@vp.pl>
2022-07-22 18:23:09 +0200

    Create a Loading spinner component and use it in other components

--- components/CardContainer/CardContainer.tsx
+++ components/CardContainer/CardContainer.tsx
@@ -2,27 +2,31 @@ import { ReactElement } from 'react';
 import { RootState } from 'store/store';
 import { useSelector } from 'react-redux';
 import Card from 'components/elements/Card/Card';
-import styles from './CardContainer.module.scss';
+import LoadingSpinner from 'components/LoadingSpinner/LoadingSpinner';
 import { Plant } from 'types/types';
+import styles from './CardContainer.module.scss';
 
 export default function CardContainer(): ReactElement {
   const plants = useSelector((state: RootState) => state.plants.plantsData);
+
   return (
     <div className={styles.cardContainer}>
-      {plants.length > 0
-        ? plants.map(({ id, name, image, description, price }: Plant) => {
-            return (
-              <Card
-                id={id}
-                key={id}
-                name={name}
-                image={image}
-                description={description}
-                price={price}
-              />
-            );
-          })
-        : 'Loading...'}
+      {plants.length > 0 ? (
+        plants.map(({ id, name, image, description, price }: Plant) => {
+          return (
+            <Card
+              id={id}
+              key={id}
+              name={name}
+              image={image}
+              description={description}
+              price={price}
+            />
+          );
+        })
+      ) : (
+        <LoadingSpinner />
+      )}
     </div>
   );
 }
