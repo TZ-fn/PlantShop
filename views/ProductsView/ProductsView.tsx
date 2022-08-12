8674162e94f3a831e526f06d25639425d35e6e20
Tomek <tomekz@vp.pl>
2022-08-12 16:32:26 +0200

    Add a list of products to the Products view

--- views/ProductsView/ProductsView.tsx
+++ views/ProductsView/ProductsView.tsx
@@ -1,9 +1,35 @@
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/store';
+import Product from 'components/ProductsList/Product/Product';
+import ProductsList from 'components/ProductsList/ProductsList';
 import styles from './ProductsView.module.scss';
 
 export default function ProductsView() {
+  const plants = useSelector((state: RootState) => state.plants.plantsData);
+
   return (
     <div className={styles.productsContainer}>
       <h2 className={styles.sectionHeader}>Products</h2>
+      <ProductsList>
+        {plants.length > 0 ? (
+          plants
+            .filter((plant) => plants.find((product) => product.id === plant.id))
+            .map(({ id, name, image, description, price }: Plant) => {
+              return (
+                <Product
+                  id={id}
+                  key={id}
+                  name={name}
+                  image={image}
+                  description={description}
+                  price={price}
+                />
+              );
+            })
+        ) : (
+          <p className={styles.emptyBasket}>Your basket is empty. ☹️</p>
+        )}
+      </ProductsList>
     </div>
   );
 }
