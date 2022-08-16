4580a828d320e54ffd077e87a4d95a244c971e34
Tomek <tomekz@vp.pl>
2022-08-16 19:33:27 +0200

    Fix an error with wrong props type name

--- components/elements/ProductMiniature/ProductMiniature.tsx
+++ components/elements/ProductMiniature/ProductMiniature.tsx
@@ -2,11 +2,11 @@ import { ReactElement } from 'react';
 import Image from 'next/image';
 import styles from './ProductMiniature.module.scss';
 
-interface BasketBadgeProps {
+interface ProductMiniatureProps {
   source: string;
 }
 
-export default function ProductMiniature({ source }: BasketBadgeProps): ReactElement {
+export default function ProductMiniature({ source }: ProductMiniatureProps): ReactElement {
   return (
     <div className={styles.miniatureContainer}>
       <Image src={source} height={96} width={80} />
