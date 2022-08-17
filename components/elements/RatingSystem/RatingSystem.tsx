5e32cf09a081c9c313b2ce3683220405ff99f2aa
Tomek <tomekz@vp.pl>
2022-08-17 21:13:36 +0200

    Add styling to the RatingSystem component

--- components/elements/RatingSystem/RatingSystem.tsx
+++ components/elements/RatingSystem/RatingSystem.tsx
@@ -9,17 +9,22 @@ interface RatingSystemProps {
 export default function RatingSystem({ rating, orientation }: RatingSystemProps): ReactElement {
   return (
     <div className={styles[`ratingContainer${orientation}`]}>
-      <div className='rating'>
-        <input type='radio' id='star5' name='rating' value='5' />
-        <label htmlFor='star5'></label>
-        <input type='radio' id='star4' name='rating' value='4' />
-        <label htmlFor='star4'></label>
-        <input type='radio' id='star3' name='rating' value='3' />
-        <label htmlFor='star3'></label>
-        <input type='radio' id='star2' name='rating' value='2' />
-        <label htmlFor='star2'></label>
-        <input type='radio' id='star1' name='rating' value='1' />
+      <div
+        className={styles.rating}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <input type='checkbox' id='star1' name='rating' />
         <label htmlFor='star1'></label>
+        <input type='checkbox' id='star2' name='rating' />
+        <label htmlFor='star2'></label>
+        <input type='checkbox' id='star3' name='rating' />
+        <label htmlFor='star3'></label>
+        <input type='checkbox' id='star4' name='rating' />
+        <label htmlFor='star4'></label>
+        <input type='checkbox' id='star5' name='rating' />
+        <label htmlFor='star5'></label>
       </div>
     </div>
   );
