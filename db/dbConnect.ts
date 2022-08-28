73630daa26a3aeb0b73bd5d7da35ab12708bf70b
Tomek <tomekz@vp.pl>
2022-08-28 21:14:15 +0200

    Add a function handling the database connection

---
+++ db/dbConnect.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+export async function dbConnect() {
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Successfully connected to MongoDB Atlas!');
+    })
+    .catch((error) => {
+      console.log('Unable to connect to MongoDB Atlas!');
+      console.error(error);
+    });
+}
