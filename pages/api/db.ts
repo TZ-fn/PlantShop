8db29ab86b1a0a48fc2e98279e49fdf596b0b2ce
Tomek <tomekz@vp.pl>
2022-09-01 21:27:23 +0200

    Fix a TS error by correctly typing server's request and response

--- pages/api/db.ts
+++ pages/api/db.ts
@@ -1,16 +1,17 @@
-import User from '../../models/User';
+import { NextApiRequest, NextApiResponse } from 'next';
 import nextConnect from 'next-connect';
+import User from '../../models/User';
 import dbConnect from 'db/dbConnect';
 
 const handler = nextConnect();
 
-handler.get(async (req, res) => {
+handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
   await dbConnect();
   const users = await User.find();
   res.json(users);
 });
 
-handler.post(async (req, res) => {
+handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
   await dbConnect();
   const user = await User.create(req.body);
   res.status(201).json({ success: true, data: user });
