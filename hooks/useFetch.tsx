c4ac03053b352418abc5cd54d7eb99653085169a
Tomek <tomekz@vp.pl>
2022-09-02 20:44:15 +0200

    Add an optional 'options' argument to enhance the useFetch hook

--- hooks/useFetch.tsx
+++ hooks/useFetch.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import { PlantsData } from 'types/types';
 
-export const useFetch = (APIurl: string): PlantsData => {
+export const useFetch = (APIurl: string, requestOptions?: Partial<RequestInit>): PlantsData => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(APIurl);
+        const response = await fetch(APIurl, requestOptions);
         const data = await response.json();
 
         if (response.status === 404) {
