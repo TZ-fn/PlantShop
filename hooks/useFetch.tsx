6bd9cc03a05d37a2ca77e7f01d361b7bc95c7e73
Tomek <tomekz@vp.pl>
2022-09-05 22:50:51 +0200

    Add variable to prevent state update if the component is unmounted

--- hooks/useFetch.tsx
+++ hooks/useFetch.tsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { PlantsData } from 'types/types';
 
-export const useFetch = (APIurl: string, requestOptions?: Partial<RequestInit>): PlantsData => {
+export const useFetch = (APIurl: string, requestOptions?: RequestInit): PlantsData => {
+  const cancelRequest = useRef<boolean>(false);
   const [data, setData] = useState([]);
+  cancelRequest.current = false;
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -12,14 +14,25 @@ export const useFetch = (APIurl: string, requestOptions?: Partial<RequestInit>):
         if (response.status === 404) {
           throw new Error(data?.message);
         }
+
+        if (cancelRequest.current) {
+          return;
+        }
         setData(data);
       } catch (error) {
+        if (cancelRequest.current) {
+          return;
+        }
         if (error instanceof Error) {
           console.log(error);
         }
       }
     };
     fetchData();
+
+    return () => {
+      cancelRequest.current = true;
+    };
   }, [APIurl]);
 
   return data;
