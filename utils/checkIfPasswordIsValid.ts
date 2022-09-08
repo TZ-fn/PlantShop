1fa036444077b6c82fbcced723cf510771d3f655
Tomek <tomekz@vp.pl>
2022-09-08 22:04:02 +0200

    Rename the validating function, add more check to it

---
+++ utils/checkIfPasswordIsValid.ts
@@ -0,0 +1,24 @@
+export default function checkIfPasswordIsValid(password: string): boolean {
+  const passwordSigns = [...password];
+  const lowerCaseLetters = /[a-z]/g;
+  const upperCaseLetters = /[A-Z]/g;
+  const numbers = /[0-9]/g;
+
+  if (passwordSigns.length < 12) {
+    return false;
+  }
+
+  if (!password.match(lowerCaseLetters)) {
+    return false;
+  }
+
+  if (!password.match(upperCaseLetters)) {
+    return false;
+  }
+
+  if (!password.match(numbers)) {
+    return false;
+  }
+
+  return true;
+}
--- utils/isPassowordValid.ts
+++
@@ -1,7 +0,0 @@
-export default function isPassowordValid(password: string): boolean {
-  const passwordSigns = [...password];
-  if (passwordSigns.length < 12) {
-    return false;
-  }
-  return true;
-}
