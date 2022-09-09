608588858bb4adcafd64e547b6396790ab5e9791
Tomek <tomekz@vp.pl>
2022-09-09 17:52:17 +0200

    Add a change function props to the Input element

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,3 +1,4 @@
+import { ChangeEventHandler } from 'react';
 import styles from './Input.module.scss';
 
 interface InputProps {
@@ -5,15 +6,22 @@ interface InputProps {
   type: string;
   placeholder: string;
   label: string;
+  onChangeFunction: ChangeEventHandler<HTMLInputElement>;
 }
 
-export default function Input({ id, type, placeholder, label }: InputProps) {
+export default function Input({ id, type, placeholder, label, onChangeFunction }: InputProps) {
   return (
     <div className={styles.inputContainer}>
       <label htmlFor={id} className={styles.inputLabel}>
         {label}
       </label>
-      <input id={id} className={styles.input} type={type} placeholder={placeholder} />
+      <input
+        id={id}
+        className={styles.input}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChangeFunction}
+      />
     </div>
   );
 }
