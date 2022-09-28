f19c416855ef0dbee3882641a87274e71c1ca7d0
Tomek <tomekz@vp.pl>
2022-09-28 15:08:13 +0100

    Remove obsolete inner divs

--- components/elements/FormLabel/FormLabel.tsx
+++ components/elements/FormLabel/FormLabel.tsx
@@ -9,13 +9,13 @@ export default function FormLabel({ type }: FormLabelProps): ReactElement {
   return (
     <div className={styles.labelContainer}>
       {type === 'wrong-email' && (
-        <div className={styles.labelContainer}>Please enter a valid e-mail address.</div>
+        <p>Please enter a valid e-mail address.</p>
       )}
       {type === 'wrong-password' && (
-        <div className={styles.labelContainer}>Please enter a valid password.</div>
+        <p>Please enter a valid password.</p>
       )}
       {type === 'no-match-password' && (
-        <div className={styles.labelContainer}>Passwords you have entered aren't matching.</div>
+        <p>Passwords you have entered aren't matching.</p>
       )}
     </div>
   );
