1e78f4f37f79de677a4531140418ab0bd5bad64e
Tomek <tomekz@vp.pl>
2022-10-23 18:36:28 +0200

    Remove an obsolete console.log and add a key property to the products' list

--- views/ProductsView/ProductsView.tsx
+++ views/ProductsView/ProductsView.tsx
@@ -6,15 +6,13 @@ import styles from './ProductsView.module.scss';
 
 export default function ProductsView() {
   const plants = useSelector((state: RootState) => state.plants.plantsData);
-
-  console.log(plants);
   return (
     <div className={styles.productsContainer}>
       <h2 className={styles.sectionHeader}>Products</h2>
       <ProductsList>
         {plants &&
           plants.map((plant) => {
-            return <Product {...plant} />;
+            return <Product {...plant} key={plant.id} />;
           })}
       </ProductsList>
     </div>
