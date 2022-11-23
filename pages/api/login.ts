53091539e65ade144f6d122d12c83b9d35c667c6
Tomek <tomekz@vp.pl>
2022-11-23 21:59:34 +0100

    Create a login route

---
+++ pages/api/login.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nextConnect from 'next-connect';
+import User from '../../models/User';
+import dbConnect from 'db/dbConnect';
+import bcrypt from 'bcrypt';
+
+const SALT_WORK_FACTOR = 10;
+
+const handler = nextConnect();
+
+handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+  await dbConnect();
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ success: true, data: user });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+export default handler;
