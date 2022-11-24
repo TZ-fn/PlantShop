b432b8a7d7f8dd5a75819b1e56fc4f835ddbf73b
Tomek <tomekz@vp.pl>
2022-11-24 23:07:01 +0100

    Add password salting when a user is created

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -2,7 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import nextConnect from 'next-connect';
 import User from '../../models/User';
 import dbConnect from 'db/dbConnect';
-
 import bcrypt from 'bcrypt';
 
 const SALT_WORK_FACTOR = 10;
@@ -10,9 +9,14 @@ const SALT_WORK_FACTOR = 10;
 const handler = nextConnect();
 
 handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  const password = req.body.password;
+  const saltedPassword = await bcrypt.hash(password, salt);
+
   await dbConnect();
+
   try {
-    const user = await User.create(req.body);
+    const user = await User.create({ ...req.body, password: saltedPassword });
     res.status(201).json({ success: true, data: user });
   } catch (e) {
     res.status(500).json(e);
