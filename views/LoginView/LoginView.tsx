2af7f77158c66425fbbc6ff3acb5abc6055b2120
Tomek <tomekz@vp.pl>
2022-11-28 18:49:19 +0100

    Move the register logic to a separate component

--- views/LoginView/LoginView.tsx
+++ views/LoginView/LoginView.tsx
@@ -1,45 +1,14 @@
-import { useState, MouseEvent, ChangeEvent, useEffect } from 'react';
+import { useState, MouseEvent, useEffect } from 'react';
 import { toast } from 'react-toastify';
-import { useFetch } from 'hooks/useFetch';
-import Input from 'components/Input/Input';
 import checkIfEmailIsValid from 'utils/checkIfEmailIsValid';
 import checkIfPasswordIsValid from 'utils/checkIfPasswordIsValid';
 import comparePasswords from 'utils/comparePasswords';
-import FormLabel from 'components/elements/FormLabel/FormLabel';
+import RegisterForm from './RegisterForm/RegisterForm';
+import LoginForm from './LoginForm/LoginForm';
 import styles from './LoginView.module.scss';
 
 export default function LoginView() {
   const [isLoginPage, setIsLoginPage] = useState(false);
-  const [registerPageValues, setRegisterPageValues] = useState({
-    email: { value: '', wasTouched: false },
-    password: { value: '', wasTouched: false },
-    confirmPassword: { value: '', wasTouched: false },
-  });
-  const [loginPageValues, setLoginPageValues] = useState({
-    email: { value: '', wasTouched: false },
-    password: { value: '', wasTouched: false },
-  });
-  const [isEmailValid, setIsEmailValid] = useState(true);
-  const [isPasswordValid, setIsPasswordValid] = useState(true);
-  const [arePasswordsMatching, setArePasswordsMatching] = useState(true);
-
-  const userData = {
-    email: registerPageValues.email.value,
-    password: registerPageValues.password.value,
-  };
-
-  const fetchSettings = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method: 'POST',
-    body: JSON.stringify(userData),
-  };
-
-  const [registerResponse, isLoading, error, registerRefresh] = useFetch(
-    '/api/register',
-    fetchSettings,
-  );
 
   function handleTabButtonClick(e: MouseEvent<HTMLButtonElement>) {
     return (e.target as HTMLButtonElement).id === 'registerTab'
@@ -85,27 +54,6 @@ export default function LoginView() {
     console.log(error);
   }, [error]);
 
-  function createUser() {
-    if (
-      !isPasswordValid ||
-      !arePasswordsMatching ||
-      !isEmailValid ||
-      !registerPageValues.email.value ||
-      !registerPageValues.password.value ||
-      !registerPageValues.confirmPassword.value
-    ) {
-      return;
-    }
-
-    refresh();
-
-    setRegisterPageValues({
-      email: { value: '', wasTouched: false },
-      password: { value: '', wasTouched: false },
-      confirmPassword: { value: '', wasTouched: false },
-    });
-  }
-
   return (
     <div className={styles.mainContainer}>
       <div className={styles.tabsContainer}>
@@ -126,57 +74,7 @@ export default function LoginView() {
           Login
         </button>
       </div>
-      {!isLoginPage && (
-        <div className={styles.registerContainer}>
-          {!isEmailValid && <FormLabel type='wrong-email' />}
-          <Input
-            id='email'
-            type='text'
-            placeholder='Enter your email...'
-            label='E-mail'
-            value={registerPageValues.email.value}
-            onChangeFunction={(e: ChangeEvent<HTMLInputElement>) =>
-              setRegisterPageValues({
-                ...registerPageValues,
-                email: { value: e.target.value, wasTouched: true },
-              })
-            }
-          />
-
-          {!isPasswordValid && <FormLabel type='wrong-password' />}
-          <Input
-            id='password'
-            type='password'
-            placeholder='Enter your password...'
-            label='Password'
-            value={registerPageValues.password.value}
-            onChangeFunction={(e: ChangeEvent<HTMLInputElement>) =>
-              setRegisterPageValues({
-                ...registerPageValues,
-                password: { value: e.target.value, wasTouched: true },
-              })
-            }
-          />
-
-          {!arePasswordsMatching && <FormLabel type='no-match-password' />}
-          <Input
-            id='confirm-password'
-            type='password'
-            placeholder='Confirm your password...'
-            label='Confirm password'
-            value={registerPageValues.confirmPassword.value}
-            onChangeFunction={(e: ChangeEvent<HTMLInputElement>) =>
-              setRegisterPageValues({
-                ...registerPageValues,
-                confirmPassword: { value: e.target.value, wasTouched: true },
-              })
-            }
-          />
-          <button type='button' className={styles.loginButton} onClick={() => createUser()}>
-            Sign me up!
-          </button>
-        </div>
-      )}
+      {!isLoginPage && <RegisterForm />}
       {isLoginPage && <LoginForm />}
     </div>
   );
---
+++ views/LoginView/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,108 @@
+import { ChangeEvent, ReactElement, useState } from 'react';
+import Input from 'components/Input/Input';
+import FormLabel from 'components/elements/FormLabel/FormLabel';
+import { useFetch } from 'hooks/useFetch';
+import styles from './LoginForm.module.scss';
+
+interface LoginFormProps {
+  children: JSX.IntrinsicElements['li'];
+}
+
+export default function LoginForm(): ReactElement {
+  const [registerPageValues, setRegisterPageValues] = useState({
+    email: { value: '', wasTouched: false },
+    password: { value: '', wasTouched: false },
+    confirmPassword: { value: '', wasTouched: false },
+  });
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [arePasswordsMatching, setArePasswordsMatching] = useState(true);
+
+  const userData = {
+    email: registerPageValues.email.value,
+    password: registerPageValues.password.value,
+  };
+
+  const fetchSettings = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(userData),
+  };
+
+  const [response, isLoading, error, refresh] = useFetch('/api/login', fetchSettings);
+
+  function createUser() {
+    if (
+      !isPasswordValid ||
+      !arePasswordsMatching ||
+      !isEmailValid ||
+      !registerPageValues.email.value ||
+      !registerPageValues.password.value ||
+      !registerPageValues.confirmPassword.value
+    ) {
+      return;
+    }
+
+    refresh();
+
+    setRegisterPageValues({
+      email: { value: '', wasTouched: false },
+      password: { value: '', wasTouched: false },
+      confirmPassword: { value: '', wasTouched: false },
+    });
+  }
+
+  return (
+    <div className={styles.registerContainer}>
+      {!isEmailValid && <FormLabel type='wrong-email' />}
+      <Input
+        id='email'
+        type='text'
+        placeholder='Enter your email...'
+        label='E-mail'
+        value={registerPageValues.email.value}
+        onChangeFunction={(e: ChangeEvent<HTMLInputElement>) =>
+          setRegisterPageValues({
+            ...registerPageValues,
+            email: { value: e.target.value, wasTouched: true },
+          })
+        }
+      />
+
+      {!isPasswordValid && <FormLabel type='wrong-password' />}
+      <Input
+        id='password'
+        type='password'
+        placeholder='Enter your password...'
+        label='Password'
+        value={registerPageValues.password.value}
+        onChangeFunction={(e: ChangeEvent<HTMLInputElement>) =>
+          setRegisterPageValues({
+            ...registerPageValues,
+            password: { value: e.target.value, wasTouched: true },
+          })
+        }
+      />
+
+      {!arePasswordsMatching && <FormLabel type='no-match-password' />}
+      <Input
+        id='confirm-password'
+        type='password'
+        placeholder='Confirm your password...'
+        label='Confirm password'
+        value={registerPageValues.confirmPassword.value}
+        onChangeFunction={(e: ChangeEvent<HTMLInputElement>) =>
+          setRegisterPageValues({
+            ...registerPageValues,
+            confirmPassword: { value: e.target.value, wasTouched: true },
+          })
+        }
+      />
+      <button type='button' className={styles.loginButton} onClick={() => createUser()}>
+        Sign me up!
+      </button>
+    </div>
+  );
+}
