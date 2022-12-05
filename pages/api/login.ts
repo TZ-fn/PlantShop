7629b0eb10cee6c207da448d20a6c41e16c22f21
Tomek <tomekz@vp.pl>
2022-12-05 22:38:13 +0100

    Add bcrypt password hashing in the /login route

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -9,10 +9,15 @@ const SALT_WORK_FACTOR = 10;
 const handler = nextConnect();
 
 handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+  const password = req.body.password;
+  const saltedPassword = await bcrypt.hash(password, salt);
+
   await dbConnect();
+
   try {
     const user = await User.findOne(req.body.email);
-    const isPasswordCorrect = user.password === req.body.password;
+    const isPasswordCorrect = user.password === saltedPassword;
     isPasswordCorrect
       ? res.status(200).json('Password correct')
       : res.status(401).json('Password incorrect');
