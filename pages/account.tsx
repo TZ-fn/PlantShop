90316392841790f29038add174d68df3eeaa91de
Tomek <tomekz@vp.pl>
2022-12-12 22:38:55 +0100

    feat: create an Account page for logged in users

---
+++ pages/account.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import AccountView from 'views/AccountView/AccountView';
+
+const Account: NextPage = () => {
+  return <AccountView />;
+};
+
+export default Account;
---
+++ views/AccountView/AccountView.tsx
@@ -0,0 +1,9 @@
+import styles from './AccountView.module.scss';
+
+export default function AccountView() {
+  return (
+    <div className={styles.accountContainer}>
+      <h2 className={styles.accountHeader}>Account</h2>
+    </div>
+  );
+}
