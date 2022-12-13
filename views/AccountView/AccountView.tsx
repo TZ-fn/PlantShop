188e74916f2cedc94b23c844d432fc61871956e5
Tomek <tomekz@vp.pl>
2022-12-13 17:56:07 +0100

    feat: create a default message for the /account route when the user is not logged in

--- views/AccountView/AccountView.tsx
+++ views/AccountView/AccountView.tsx
@@ -1,9 +1,14 @@
+import { RootState } from 'store/store';
+import { useSelector } from 'react-redux';
 import styles from './AccountView.module.scss';
 
 export default function AccountView() {
+  const isUserAuthorised = useSelector((state: RootState) => state.authorisation.isUserAuthorised);
+
   return (
     <div className={styles.accountContainer}>
       <h2 className={styles.accountHeader}>Account</h2>
+      {isUserAuthorised ? '' : <p className={styles.notLoggedIn}>Please log in first...</p>}
     </div>
   );
 }
