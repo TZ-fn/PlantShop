90ef9b4e6c9bd0f565a5f207bd0262e96b65becc
Tomek <tomekz@vp.pl>
2022-12-14 19:10:09 +0100

    feat: display a message instead of the login/register forms when the user is already logged in

--- views/LoginView/LoginView.tsx
+++ views/LoginView/LoginView.tsx
@@ -1,18 +1,29 @@
 import { useState, MouseEvent } from 'react';
+import Image from 'next/image';
+import AccountIcon from 'public/icons/loginIcon.svg';
+import { RootState } from 'store/store';
+import { useSelector } from 'react-redux';
 import RegisterForm from './RegisterForm/RegisterForm';
 import LoginForm from './LoginForm/LoginForm';
 import styles from './LoginView.module.scss';
 
 export default function LoginView() {
   const [isLoginPage, setIsLoginPage] = useState(false);
+  const isUserAuthorised = useSelector((state: RootState) => state.authorisation.isUserAuthorised);
 
   function handleTabButtonClick(e: MouseEvent<HTMLButtonElement>) {
     return (e.target as HTMLButtonElement).id === 'registerTab'
       ? setIsLoginPage(false)
       : setIsLoginPage(true);
   }
+  console.log(isUserAuthorised);
 
-  return (
+  return isUserAuthorised ? (
+    <div className={styles.loggedIn}>
+      <Image src={AccountIcon.src} width={120} height={120} />
+      <p>You are already logged in.</p>
+    </div>
+  ) : (
     <div className={styles.mainContainer}>
       <div className={styles.tabsContainer}>
         <button
