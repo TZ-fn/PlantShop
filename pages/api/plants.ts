ee515dabf2478d4f09ac81a5dcd4801bfc90eb07
Tomek <tomekz@vp.pl>
2022-12-22 19:37:29 +0100

    fix: fix an unused parameter error

--- pages/api/plants.ts
+++ pages/api/plants.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { PlantsData } from '../../types/types';
 import plantsData from '../../public/data/plantsData';
 
-export default function handler(request: NextApiRequest, response: NextApiResponse<PlantsData>) {
+export default function handler(_request: NextApiRequest, response: NextApiResponse<PlantsData>) {
   response.status(200).json(plantsData);
 }
