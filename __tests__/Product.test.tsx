c21598d91262f6d9ba1d437313baef4c1a085586
Tomek <tomekz@vp.pl>
2023-01-06 14:29:37 +0100

    feat: add user-event library to the project, add more tests to the Product component

--- __tests__/Product.test.tsx
+++ __tests__/Product.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { store } from 'store/store';
 import { updatePlantsData } from 'features/plants/plantsSlice';
@@ -9,16 +10,34 @@ import Product from 'components/ProductsList/Product/Product';
 describe('test the Product component', () => {
   const plants = plantsData;
   const { id, name, image, price, description } = plants[0];
+  const user = userEvent.setup();
 
   store.dispatch(updatePlantsData(plants));
 
   render(
     <Provider store={store}>
-      <Product id={id} name={name} image={image} price={price} description={description} />
+      <Product
+        id={id}
+        name={name}
+        image={image}
+        price={price}
+        description={description}
+        isInBasket
+      />
     </Provider>,
   );
 
+  const incrementButton = screen.getByText('-');
+  const decrementButton = screen.getByText('+');
+  const itemCount = screen.getByText('Count of the item');
+
   it('renders correctly', () => {
     expect(screen.getByText(name)).toBeInTheDocument();
+  });
+
+  it("increments and decrements product's quantity correctly", () => {
+    user.click(incrementButton);
+    screen.debug();
+    expect(itemCount).toHaveValue(1);
   });
 });
