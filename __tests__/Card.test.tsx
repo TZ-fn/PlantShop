956a94fabd0f9375a93a66a2396a2c7e2c73d123
Tomek <tomekz@vp.pl>
2023-01-12 20:19:08 +0100

    feat: add more tests to the Card component

--- __tests__/Card.test.tsx
+++ __tests__/Card.test.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
 import { store } from 'store/store';
 import plantsData from 'public/data/plantsData';
 import Card from 'components/elements/Card/Card';
 
-it('renders the Card component correctly', () => {
-  const plants = plantsData;
-  const { id, name, image, price, description } = plants[0];
+const plants = plantsData;
+const { id, name, image, price, description } = plants[0];
 
-  const user = userEvent.setup();
+const basket = store.getState().basket.products;
 
+const user = userEvent.setup();
+
+function renderCard() {
   render(
     <Provider store={store}>
       <Card id={id} name={name} image={image} description={description} price={price} />
     </Provider>,
   );
-  expect(screen.getByText(name)).toBeInTheDocument();
+}
+
+describe('', () => {
+  it('renders the Card component correctly', () => {
+    renderCard();
+
+    expect(screen.getByText(name)).toBeInTheDocument();
+  });
+
+  it('adds product to the basket correctly', () => {
+    renderCard();
+
+    const addToBasketBtn = screen.getByTestId('basket');
+
+    user.click(addToBasketBtn);
+  });
 });
