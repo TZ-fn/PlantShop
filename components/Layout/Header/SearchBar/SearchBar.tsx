d951411d9b323bd8d9b6b03dd7eb2bb3a2968140
Tomek <tomekz@vp.pl>
2023-01-16 21:26:31 +0100

    feat: add event handling in the SearchBar component

--- components/Layout/Header/SearchBar/SearchBar.tsx
+++ components/Layout/Header/SearchBar/SearchBar.tsx
@@ -1,11 +1,27 @@
-import { ReactElement } from 'react';
+import { ChangeEvent, MouseEvent, ReactElement, useState } from 'react';
 import styles from './SearchBar.module.scss';
 
 export default function SearchBar(): ReactElement {
+  const [searchValue, setSearchValue] = useState('');
+
+  function handleSearchBar(e: ChangeEvent<HTMLInputElement>) {
+    setSearchValue(e.target.value);
+  }
+
+  function handleSearchButton(e: MouseEvent<HTMLButtonElement>) {
+    return;
+  }
+
   return (
     <div className={styles.searchContainer}>
-      <input placeholder='Search for products...' className={styles.searchInput} type='text' />
-      <button className={styles.searchButton} type='button'>
+      <input
+        placeholder='Search for products...'
+        className={styles.searchInput}
+        type='text'
+        onChange={handleSearchBar}
+        value={searchValue}
+      />
+      <button className={styles.searchButton} type='button' onClick={handleSearchButton}>
         Search
       </button>
     </div>
