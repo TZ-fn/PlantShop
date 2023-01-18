e72c9fce9af956b5827354a68b71dfd6ebe09054
Tomek <tomekz@vp.pl>
2023-01-18 20:59:30 +0100

    feat: add a filtering function to the SearchBar component

--- components/Layout/Header/SearchBar/SearchBar.tsx
+++ components/Layout/Header/SearchBar/SearchBar.tsx
@@ -1,11 +1,22 @@
 import { ChangeEvent, MouseEvent, ReactElement, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/store';
+import { PlantsData } from 'types/types';
 import styles from './SearchBar.module.scss';
 
 export default function SearchBar(): ReactElement {
   const [searchValue, setSearchValue] = useState<string>('');
+  const plants = useSelector((state: RootState) => state.plants.plantsData);
+
+  function searchPlans(plants: PlantsData, query: string) {
+    return plants
+      .filter((plant) => plant.name.toLocaleLowerCase().includes(query))
+      .map((plant) => plant.name);
+  }
 
   function handleSearchBar(e: ChangeEvent<HTMLInputElement>) {
     setSearchValue(e.target.value);
+    console.log(searchPlans(plants, e.target.value));
   }
 
   function handleSearchButton(e: MouseEvent<HTMLButtonElement>) {
