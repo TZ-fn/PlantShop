d4be14f0253c1781e7641a1113b394f18988152e
Tomek <tomekz@vp.pl>
2023-01-19 20:15:58 +0100

    feat: implement the search auto suggestions

--- components/Layout/Header/SearchBar/SearchBar.tsx
+++ components/Layout/Header/SearchBar/SearchBar.tsx
@@ -1,12 +1,17 @@
-import { ChangeEvent, MouseEvent, ReactElement, useState } from 'react';
+import { ChangeEvent, MouseEvent, KeyboardEvent, ReactElement, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 import { PlantsData } from 'types/types';
 import styles from './SearchBar.module.scss';
 
 export default function SearchBar(): ReactElement {
-  const [searchValue, setSearchValue] = useState<string>('');
   const plants = useSelector((state: RootState) => state.plants.plantsData);
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
+  const [filteredSuggestions, setFilteredSuggestions] = useState<string[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [wasArrowDownPressedAlready, setWasArrowDownPressedAlready] = useState(false);
+  const savedSearchValue = useRef(searchValue);
 
   function searchPlans(plants: PlantsData, query: string) {
     return plants
@@ -19,9 +24,65 @@ export default function SearchBar(): ReactElement {
     console.log(searchPlans(plants, e.target.value));
   }
 
-  function handleSearchButton(e: MouseEvent<HTMLButtonElement>) {
-    return;
-  }
+  const resetTheSearch = () => {
+    setSearchValue('');
+  };
+
+  const handleSearchButton = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!searchValue.trim()) {
+      return;
+    }
+    setActiveSuggestionIndex(0);
+    savedSearchValue.current = '';
+    setSearchValue('');
+    setShowSuggestions(false);
+    setWasArrowDownPressedAlready(false);
+    // router.push(`/country/${searchValue}`);
+  };
+
+  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'ArrowDown') {
+      if (!searchValue.trim()) {
+        return;
+      }
+      e.preventDefault();
+      if (wasArrowDownPressedAlready && activeSuggestionIndex === filteredSuggestions.length - 1) {
+        return;
+      }
+      if (!wasArrowDownPressedAlready) {
+        setSearchValue(filteredSuggestions[0]);
+        setWasArrowDownPressedAlready(true);
+      } else {
+        setActiveSuggestionIndex(activeSuggestionIndex + 1);
+        setSearchValue(filteredSuggestions[activeSuggestionIndex + 1]);
+      }
+    }
+
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      if (activeSuggestionIndex === 0) {
+        setSearchValue(savedSearchValue.current);
+        setWasArrowDownPressedAlready(false);
+        return;
+      }
+      setActiveSuggestionIndex(activeSuggestionIndex - 1);
+      setSearchValue(filteredSuggestions[activeSuggestionIndex - 1]);
+    }
+
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      if (!searchValue.trim()) {
+        return;
+      }
+      setActiveSuggestionIndex(0);
+      savedSearchValue.current = '';
+      setSearchValue('');
+      setShowSuggestions(false);
+      setWasArrowDownPressedAlready(false);
+      // router.push(`/country/${searchValue}`);
+    }
+  };
 
   return (
     <div className={styles.searchContainer}>
@@ -31,10 +92,50 @@ export default function SearchBar(): ReactElement {
         type='text'
         onChange={handleSearchBar}
         value={searchValue}
+        onKeyDown={(e) => handleKeyPress(e)}
+        onBlur={() => setTimeout(() => setShowSuggestions(false), 300)}
+        onFocus={() => setShowSuggestions(true)}
+        autoComplete='off'
       />
       <button className={styles.searchButton} type='button' onClick={handleSearchButton}>
         Search
       </button>
+      {searchValue?.length > 0 && showSuggestions === true ? (
+        <ul className='styles.autoSuggestionsContainer'>
+          {(() => {
+            switch (true) {
+              case searchValue.length === 1:
+                return (
+                  <li className={styles.autoSuggestionItem} key='inputValueTooShort' isActive>
+                    Please enter at least 2 letters...
+                  </li>
+                );
+              case filteredSuggestions.length === 0:
+                return (
+                  <li className={styles.autoSuggestionItem} key='NoResults' isActive>
+                    No results found...
+                  </li>
+                );
+              default:
+                return filteredSuggestions.map((country: string, index) => {
+                  let isActive = false;
+                  if (index === activeSuggestionIndex) {
+                    isActive = true;
+                  }
+                  return (
+                    <li
+                      className={styles.autoSuggestionItem}
+                      onClick={() => resetTheSearch()}
+                      key={country}
+                    >
+                      <Link href={`/country/${country.toLocaleLowerCase()}`}>{country}</Link>
+                    </li>
+                  );
+                });
+            }
+          })()}
+        </ul>
+      ) : null}
     </div>
   );
 }
