3574de1a249b8e74fe0bf92ca3a76312e1d3cc01
Tomek <tomekz@vp.pl>
2023-02-06 21:32:20 +0100

    feat: add more test to the ItemGallery component

--- __tests__/ItemGallery.test.tsx
+++ __tests__/ItemGallery.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event';
 import { store } from 'store/store';
 import plantsData from 'public/data/plantsData';
 import ItemGallery from 'components/ItemGallery/ItemGallery';
@@ -9,6 +10,7 @@ import { unsplashData } from './mocked-data/mocked-unsplash-data';
 describe('test the ItemGallery component', () => {
   const plants = plantsData;
   const { name } = plants[0];
+  const user = userEvent.setup();
 
   global.fetch = jest.fn().mockImplementation(() =>
     Promise.resolve({
@@ -16,13 +18,32 @@ describe('test the ItemGallery component', () => {
     }),
   ) as jest.Mock;
 
-  it('renders correctly', async () => {
+  function renderItemGallery() {
     render(
       <Provider store={store}>
         <ItemGallery name={name} />
       </Provider>,
     );
+  }
 
+  it('renders correctly', async () => {
+    renderItemGallery();
     expect(await screen.findAllByRole('img')).toHaveLength(9);
+  });
+
+  it('modal works correctly', async () => {
+    renderItemGallery();
+
+    const allImages = await screen.findAllByRole('img');
+    const firstImage = allImages[0];
+
+    await user.click(firstImage);
+
+    const nextPhotoBtn = await screen.findByRole('button', { name: />>/i });
+    const prevPhotoBtn = await screen.findByRole('button', { name: /<</i });
+
+    // look for modal navigation's buttons
+    expect(nextPhotoBtn).toBeInTheDocument();
+    expect(prevPhotoBtn).toBeInTheDocument();
   });
 });
