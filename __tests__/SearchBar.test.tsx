63287ceb1c66f4e40949973af286eff5ef746aa6
Tomek <tomekz@vp.pl>
2023-02-13 21:32:10 +0100

    feat: add more tests to the SearchBar component

--- __tests__/SearchBar.test.tsx
+++ __tests__/SearchBar.test.tsx
@@ -4,12 +4,14 @@ import plantsData from 'public/data/plantsData';
 import userEvent from '@testing-library/user-event';
 import { store } from 'store/store';
 import { Provider } from 'react-redux';
+import { updatePlantsData } from 'features/plants/plantsSlice';
 import SearchBar from 'components/Layout/Header/SearchBar/SearchBar';
 
 describe('test SearchBar', () => {
-  // const plants = plantsData;
-  // const { id } = plants[0];
-  // const user = userEvent.setup();
+  const plants = plantsData;
+  store.dispatch(updatePlantsData(plants));
+
+  const user = userEvent.setup();
 
   function renderSearchBar() {
     render(
@@ -17,10 +19,42 @@ describe('test SearchBar', () => {
         <SearchBar />
       </Provider>,
     );
+
+    const searchInput = screen.getByRole('textbox');
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    return { searchInput, searchButton };
   }
 
-  it('renders the button correctly', () => {
-    renderSearchBar();
-    expect(screen.getByPlaceholderText(/Search for products/)).toBeInTheDocument();
+  it('renders the search bar correctly', () => {
+    const { searchInput, searchButton } = renderSearchBar();
+
+    expect(searchInput).toBeInTheDocument();
+    expect(searchButton).toBeInTheDocument();
+  });
+
+  it('shows the autocomplete items correctly', async () => {
+    const { searchInput } = renderSearchBar();
+
+    await user.type(searchInput, 'ca');
+
+    // look for plants that have "ca" in their names
+    expect(await screen.findByText(/Zamioculcas/)).toBeInTheDocument();
+    expect(await screen.findByText(/Dracaena/)).toBeInTheDocument();
+    expect(await screen.findByText(/Calathea/)).toBeInTheDocument();
+  });
+
+  it('shows the autocomplete errors correctly', async () => {
+    const { searchInput } = renderSearchBar();
+
+    await user.type(searchInput, 'c');
+
+    // look for "query too short" error
+    expect(await screen.findByText(/Please enter at least 2 letters.../)).toBeInTheDocument();
+
+    await user.type(searchInput, 'ccccc');
+
+    // look for "no items found" error
+    expect(await screen.findByText(/No results found.../)).toBeInTheDocument();
   });
 });
