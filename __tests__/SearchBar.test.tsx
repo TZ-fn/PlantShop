38c940b52bb8f9f6c2f4e236e585eb8ff507ed62
Tomek <tomekz@vp.pl>
2023-02-16 17:47:10 +0100

    feat: add more tests to the SearchBar component

--- __tests__/SearchBar.test.tsx
+++ __tests__/SearchBar.test.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { createRouterMock } from './mocked-data/createRouterMock';
 import plantsData from 'public/data/plantsData';
 import userEvent from '@testing-library/user-event';
 import { store } from 'store/store';
 import { Provider } from 'react-redux';
 import { updatePlantsData } from 'features/plants/plantsSlice';
 import SearchBar from 'components/Layout/Header/SearchBar/SearchBar';
+
+const router = createRouterMock();
 
 describe('test SearchBar', () => {
   const plants = plantsData;
@@ -15,9 +19,11 @@ describe('test SearchBar', () => {
 
   function renderSearchBar() {
     render(
-      <Provider store={store}>
-        <SearchBar />
-      </Provider>,
+      <RouterContext.Provider value={router}>
+        <Provider store={store}>
+          <SearchBar />
+        </Provider>
+      </RouterContext.Provider>,
     );
 
     const searchInput = screen.getByRole('textbox');
@@ -75,5 +81,31 @@ describe('test SearchBar', () => {
     await user.type(searchInput, '{arrowUp}');
     await user.type(searchInput, '{arrowUp}');
     expect(searchInput).toHaveValue('ca');
+  });
+
+  it('handles the search button correctly', async () => {
+    const { searchInput, searchButton } = renderSearchBar();
+    const searchValue = 'ca';
+
+    await user.type(searchInput, searchValue);
+    // press the arrow down to select the first autosuggestion - "Zamioculcas"
+    await user.type(searchInput, '{arrowDown}');
+
+    await user.click(searchButton);
+
+    expect(router.push).toHaveBeenCalledWith(`/product/zamioculcas`);
+  });
+
+  it('handles the Enter keypress correctly', async () => {
+    const { searchInput } = renderSearchBar();
+    const searchValue = 'ca';
+
+    await user.type(searchInput, searchValue);
+    // press the arrow down to select the first autosuggestion - "Zamioculcas"
+    await user.type(searchInput, '{arrowDown}');
+
+    await user.type(searchInput, '{Enter}');
+
+    expect(router.push).toHaveBeenCalledWith(`/product/zamioculcas`);
   });
 });
