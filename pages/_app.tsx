623e47e08ba8a7d69505c8c4f8b1c818771ea559
Tomek <tomekz@vp.pl>
2023-07-10 20:30:45 +0200

    fix: add missing types for the next-auth session props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,7 +9,12 @@ import Layout from 'components/Layout/Layout';
 import 'styles/globals.css';
 import 'styles/variables.scss';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
   return (
     <Provider store={store}>
       <SessionProvider session={pageProps.session}>
