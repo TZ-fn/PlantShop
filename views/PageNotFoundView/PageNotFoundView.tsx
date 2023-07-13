16718067fa2ec522c2d87168922464d0c6577d06
Tomek <tomekz@vp.pl>
2023-07-13 19:39:47 +0200

    feat: add styling to the 404 page, change the default font to Lato

--- views/PageNotFoundView/PageNotFoundView.tsx
+++ views/PageNotFoundView/PageNotFoundView.tsx
@@ -1,10 +1,18 @@
-import type { NextPage } from 'next';
+import Link from 'next/link';
 import styles from './PageNotFoundView.module.scss';
 
-const PageNotFoundView: NextPage = () => {
+const PageNotFoundView = () => {
   return (
-    <div className={styles.container}>
-      <h1>404 - Page Not Found</h1>
+    <div className={styles.mainContainer}>
+      <h2 className={styles.sectionHeader}>404 - Page Not Found</h2>
+      <p className={styles.message}>The address you are trying to reach doesn't exist. ☹️</p>
+      <p className={styles.message}>
+        Return to the{' '}
+        <Link href={'/'} passHref>
+          <span className={styles.link}>main page</span>
+        </Link>
+        .
+      </p>
     </div>
   );
 };
