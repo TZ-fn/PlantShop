40cd56109fe73276a091e7399601e775f615aac0
Tomek <tomekz@vp.pl>
2023-07-18 21:15:43 +0200

    chore: remove an obsolete  console.log

--- views/AccountView/AccountDetails/AccountDetails.tsx
+++ views/AccountView/AccountDetails/AccountDetails.tsx
@@ -4,7 +4,7 @@ import styles from './AccountDetails.module.scss';
 
 export default function AccountDetails() {
   const username = useSelector((state: RootState) => state.authorisation.username);
-  console.log(username);
+
   return (
     <div className={styles.accountDetailsContainer}>
       <p className={styles.userName}>You are logged in as: {username}</p>
